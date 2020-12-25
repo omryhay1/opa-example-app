1921b289647eaae1759243d4cb7d01013aa891bc
Tim Davis <tim.davis.instinct@gmail.com>
2020-12-25 09:18:58 -0600

    Publishing App

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+  description = "AWS region to launch servers."
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  type = string
+  default = ""
+}
+
+# Ubuntu Precise 12.04 LTS (x64)
+variable "aws_amis" {
+  default = {
+    eu-west-1 = "ami-674cbc1e"
+    us-east-1 = "ami-1d4e7a66"
+    us-west-1 = "ami-969ab1f6"
+    us-west-2 = "ami-8803e0f0"
+  }
+}
